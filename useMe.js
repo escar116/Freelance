1f069396ea155d7046e30fada53b76e2bba153b2
escar116 <charlesjanparaggua@gmail.com>
2026-08-15 14:07:43 +0800

    Fix useMe.js to use AuthContext instead of mockDb

--- useMe.js
+++ useMe.js
@@ -1,11 +1,10 @@
-import { db } from "./mockDb";
-
-import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "./AuthContext";
 
 export default function useMe() {
-  return useQuery({
-    queryKey: ["me"],
-    queryFn: () => db.auth.me(),
-    staleTime: 60000,
-  });
+  const { user, isLoadingAuth } = useAuth();
+  
+  return {
+    data: user,
+    isLoading: isLoadingAuth,
+  };
 }
